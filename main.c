c5ba7777ea1b330c0fbca3319f777cad17f68b48
JamesRoyBoogaard <jamesboogaard66@gmail.com>
2025-12-09 16:11:16 +0100

    I have gotten the simple wiindow working for myself, apparently i cant make thick borders due to os interference. Just a test though. otherwise this works very nicely and is rather simple

--- main.c
+++ main.c
@@ -5,20 +5,26 @@
 
 
 int main(){
-	printf("lets no do it ourselves!!");
+	XEvent event;
 	Display *display = XOpenDisplay(NULL); 
 	if(!display){
 		fprintf(stderr, "No display server could be found");
-	}else{
-		printf("%p is the address", display);
-	}	
+	}
 	uint16_t window_width = 500;
 	uint16_t window_height = 500;
-	uint8_t border_width = 10;
+	uint8_t border_width = 230;
 	Window window = XCreateSimpleWindow(display,XDefaultRootWindow(display),50,50, window_width, window_height, border_width, XBlackPixel(display, 0), XWhitePixel(display, 0));
 	XMapWindow(display,window);
-	XSelectInput(display, window, ExposureMask);
+	XSelectInput(display, window, KeyPressMask);
+	while(1){
+		XNextEvent(display, &event);
+		switch (event.type) {
+			case KeyPress:
+				XCloseDisplay(display);
+				return 0;
+		}
+	}
 
 
 	return 1;
-}
+	}
