56df7aa732c3477c8ae1d0c5b82b24254ecfdd89
JamesRoyBoogaard <jamesboogaard66@gmail.com>
2025-12-09 16:12:29 +0100

    small updates

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int main(){
 	}
 	uint16_t window_width = 500;
 	uint16_t window_height = 500;
-	uint8_t border_width = 230;
+	uint8_t border_width = 1;
 	Window window = XCreateSimpleWindow(display,XDefaultRootWindow(display),50,50, window_width, window_height, border_width, XBlackPixel(display, 0), XWhitePixel(display, 0));
 	XMapWindow(display,window);
 	XSelectInput(display, window, KeyPressMask);
