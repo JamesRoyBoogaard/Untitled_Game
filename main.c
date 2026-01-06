45267d49d24cce995b011165af48147633327aa7
JamesRoyBoogaard <jamesboogaard66@gmail.com>
2026-01-06 15:52:29 +0100

    beginning to experiment with how a sprite works with c

--- main.c
+++ main.c
@@ -8,9 +8,17 @@ uint16_t window_height;
 uint32_t *frame_buffer;
 uint32_t *mc_sprite;
 
+struct sprite{
+	uint16_t height;
+	uint16_t width;
+	uint32_t colour;
+	uint16_t x_pos;
+	uint16_t y_pos;
+};
+
 void change_pixel_colour(uint16_t x, uint16_t y, uint32_t colour);
 Bool is_sky(uint16_t x, uint16_t y);
-void draw_sprite(uint16_t height, uint16_t width, uint32_t colour, uint16_t x_pos, uint16_t y_pos);
+Bool draw_sprite(uint16_t x_pos, uint16_t y_pos);
 
 int main(){
 
@@ -44,6 +52,10 @@ int main(){
 
 	for(uint16_t x = 0; x < window_width; x++){
 		for(uint16_t y = 0; y < window_height; y++){
+
+		if(draw_sprite(x,y)){
+
+		}
 		if(is_sky(x, y)){
 			change_pixel_colour(x , y, 0xFF00FF00);
 	
@@ -65,8 +77,6 @@ int main(){
 				break;
 		}
 	}
-
-
 	return 1;
 }
 
@@ -85,6 +95,14 @@ Bool is_sky(uint16_t x, uint16_t y){
 }
 
 
-void draw_sprite(uint16_t height, uint16_t width, uint32_t colour, uint16_t x_pos, uint16_t y_pos){
 
+Bool draw_sprite(uint16_t x_pos, uint16_t y_pos){ 
+	// There is a current x and y position for the sprite which all other pixels in the sprite then are referenced from.
+	uint16_t height, width = 50;
+	uint32_t colour = 0xFF00FF00;
+	// Essentially going to check if the current pixel is within the sprite or not. Then i guess we draw the sprite
+	if(x_pos){
+		
+	}
+	return True;
 }
